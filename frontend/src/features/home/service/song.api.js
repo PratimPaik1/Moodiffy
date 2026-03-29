89602e343147a7b3eeaa338dc002b77751cbbfde
PratimPaik1 <PratimPaik0@gmail.com>
2026-03-30 00:42:30 +0530

    fix minnor issue

--- frontend/src/features/home/service/song.api.js
+++ frontend/src/features/home/service/song.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api=axios.create({
-     baseURL: "http://localhost:3000/api",
+     baseURL: "https://moodify-2zhx.onrender.com",
     withCredentials: true
 })
 //http://localhost:3000/api/song/getSong?mood=sad
