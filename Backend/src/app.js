558bc3b1f95747f5bfe0cd346119564fb52638cc
PratimPaik1 <PratimPaik0@gmail.com>
2026-03-30 12:27:48 +0530

    static page issue fixed

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -21,6 +21,7 @@ app.use("/api/auth",authRouter)
 app.use("/api/song",songRouter)
 
 
+app.use(express.static(path.join(__dirname, "..", "public"))); 
 app.get("*name", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
